bfab6d6848ae7a467da18c65b04751d6721dbe65
Josh Summerhays <russiansummer@gmail.com>
2016-05-08 20:50:38 -0600

    updates, add jquery/underscore, css fixes

---
+++ pagePersonal.js
@@ -0,0 +1,7 @@
+/*! pagePersonal v0.0.0 - MIT license */
+// 'use strict';
+
+
+  $("#name").html("Josh");
+  
+
---
+++ spec/pagePersonal.js
@@ -0,0 +1,20 @@
+/* global pagePersonal, describe, it, expect, should */
+
+describe('pagePersonal()', function () {
+  'use strict';
+
+  it('exists', function () {
+    expect(pagePersonal).to.be.a('function');
+
+  });
+
+  it('does something', function () {
+    expect(true).to.equal(false);
+  });
+
+  it('does something else', function () {
+    expect(true).to.equal(false);
+  });
+
+  // Add more assertions here
+});
