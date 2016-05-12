96679252ae4b696f47a8a4b8d28b4bac0b0d6892
Josh Summerhays <russiansummer@gmail.com>
2016-05-11 23:35:52 -0600

    more enhancements/cleanup

--- pagePersonal.js
+++ pagePersonal.js
@@ -1,7 +1,10 @@
 /*! pagePersonal v0.0.0 - MIT license */
 // 'use strict';
+$(document).ready(function(){
 
+  $("#header").html('Josh Summerhays');
+  $('#footer').html('756 E. 8280 S., Sandy, Utah 84094 | (801) 505-2869');
 
-  $("#name").html("Josh");
+  // <p>756 E. 8280 S., Sandy, Utah 84094 | (801) 505-2869</p>
   
-
+});
